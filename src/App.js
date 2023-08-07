eaa3254b5eeea9fc74536a84aa309fc5e02e4658
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-07 19:46:18 +0530

    expenses added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Welcome to Expense Tracker</h1>
+    <ExpenseItem></ExpenseItem>
+  </div>
   );
 }
 
 export default App;
+
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <ul>
+                <h3>Expense Items</h3>
+                <li>Food Rs. 10</li>
+                <li>Petrol Rs. 100</li>
+                <li>Movies Rs. 200</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ExpenseItem
