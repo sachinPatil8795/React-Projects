1168636a72ff5f7ebd34d5d2cde631b532b412ea
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-10 01:01:02 +0530

    making UI dynamic

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./components/UI/Card";
 import "./components/Expense/ExpenseItemStyle.css";
 import Expenses from "./components/Expense/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
 const App = () => {
-  const expenses = [
-    {
-      title: "iPhone",
-      date: new Date(2023, 7, 8),
-      amount: 600
-    },
-    {
-      title: "Travel",
-      date: new Date(2023, 7, 20),
-      amount: 800
-    },
-    {
-      title: "Mercedes Benz",
-      date: new Date(2023, 11, 11),
-      amount: 50000
-    },
-    {
-      title: "House",
-      date: new Date(2023, 12, 8),
-      amount: 100000
-    },
-  ];
+  const [expenses, setExpenses] = useState([]);
+  
+  const addExpenseHandler = (expense) => {
+    setExpenses([...expenses, expense]);
+  }
+  
 
   return (
     <Card>
       <h1 className="main-header">Welcome to Expense Tracker</h1>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </Card>
   );
