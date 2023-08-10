262478236c9cc0cad0ae77c259a3631bdd42c7e9
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-10 16:35:43 +0530

    conditional rendering

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -4,23 +4,11 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
-  // const [title, setTitle] = useState(props.title);
-  // const [amount, setAmount] = useState(props.amount);
-
-  // const clickHandler = () => {
-  //   setTitle("Updated");
-  //   setAmount(100);
-
-  //   console.log("Updated Title:", title);
-  //   console.log("Updated Amount:", amount);
-  // };
-
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <ExpenseDetails title={props.title} amount={props.amount} />
-        {/* <button onClick={clickHandler}></button> */}
       </Card>
     </li>
   );
